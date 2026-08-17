45b830aa434b3d206c325c75ba89dfd3528fdd5e
irmafiguero199 <irmafiguero199@gmail.com>
2026-08-17 23:01:25 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import MainLayout from './layouts/MainLayout'
 import Home from './pages/Home'
+import About from './pages/About'
 import Donate from './pages/Donate'
 import Gallery from './pages/Gallery'
 import News from './pages/News'
@@ -16,6 +17,7 @@ function App() {
       <Routes>
         <Route element={<MainLayout />}>
           <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
           <Route path="/donate" element={<Donate />} />
           <Route path="/gallery" element={<Gallery />} />
           <Route path="/news" element={<News />} />
